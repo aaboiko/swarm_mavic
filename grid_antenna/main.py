5411320ba46bef4e714229311a561e01916f4257
aaboiko <aboiko.exceedteam@gmail.com>
2025-03-11 16:28:58 +0300

    3d animation added

--- grid_antenna/main.py
+++ grid_antenna/main.py
@@ -4,17 +4,22 @@ import matplotlib.animation as animation
 
 from utils import PointAgent, Anchor
 from matplotlib.patches import Circle
+from mpl_toolkits import mplot3d
+
 
 N_AGENTS_IN_FAMILY = 2
 N_ANCHORS = 16
 
 log_path_agents = "grid_antenna/logs/agents/log_agents_7.txt"
 log_path_anchors = "grid_antenna/logs/anchors/log_anchors_7.txt"
-gif_path = "grid_antenna/gifs/grid_antenna_7_xz.gif"
+gif_path = "grid_antenna/gifs/grid_antenna_7_3d.gif"
 
 dt = 0.02
-n_frames = 3000
+n_frames = 5000
 data = []
+
+fig_3d = plt.figure(figsize = (20, 20))
+ax_3d = fig_3d.add_subplot(projection='3d')
 
 s_point = 10
 s_anchor = 20
@@ -23,7 +28,15 @@ x_bounds = (-10, 10)
 y_bounds = (-10, 10)
 z_bounds = (0, 20)
 
-anchors_attraction_point = np.array([0, 0, 0])
+x_agent_min, x_agent_max = -10, 10
+y_agent_min, y_agent_max = -10, 10
+z_agent_min, z_agent_max = 1, 10
+
+x_anchor_min, x_anchor_max = -10, 10
+y_anchor_min, y_anchor_max = -10, 10
+z_anchor_min, z_anchor_max = 1, 3
+
+anchors_attraction_point = np.array([0, 0, 1])
 family_colors = ["green", "blue", "orange", "black", "brown", "aquamarine", "aqua", "azure", "coral", "chocolate", "purple", "teal", "pink", "gold", "violet", "magenta"]
 
 
@@ -70,8 +83,8 @@ def write_log(path, points):
 
 
 def process():
-    anchors = [Anchor(id=i, family_id=i, x=np.random.uniform(x_bounds[0], x_bounds[1]), y=np.random.uniform(y_bounds[0], y_bounds[1]), z=np.random.uniform(z_bounds[0], z_bounds[1])) for i in range(N_ANCHORS)]
-    agents = [PointAgent(id=i, family_id=(i // N_AGENTS_IN_FAMILY), x=np.random.uniform(x_bounds[0], x_bounds[1]), y=np.random.uniform(y_bounds[0], y_bounds[1]), z=np.random.uniform(z_bounds[0], z_bounds[1])) for i in range(N_ANCHORS * N_AGENTS_IN_FAMILY)]
+    anchors = [Anchor(id=i, family_id=i, x=np.random.uniform(x_anchor_min, x_anchor_max), y=np.random.uniform(y_anchor_min, y_anchor_max), z=np.random.uniform(z_anchor_min, z_anchor_max)) for i in range(N_ANCHORS)]
+    agents = [PointAgent(id=i, family_id=(i // N_AGENTS_IN_FAMILY), x=np.random.uniform(x_agent_min, x_agent_max), y=np.random.uniform(y_agent_min, y_agent_max), z=np.random.uniform(z_agent_min, z_agent_max)) for i in range(N_ANCHORS * N_AGENTS_IN_FAMILY)]
 
     n_iters = 10000
 
@@ -238,7 +251,7 @@ def animate_xz(i):
         plt.gca().add_artist(circle)
 
 
-def create_gif(log_path_agents, log_path_anchors, gif_path):
+def create_gif(log_path_agents, log_path_anchors, gif_path, flag="xy"):
     iters = 0
 
     with open(log_path_agents, "r") as f_agents, open(log_path_anchors, "r") as f_anchors:
@@ -255,11 +268,101 @@ def create_gif(log_path_agents, log_path_anchors, gif_path):
             if iters > n_frames:
                  break
             
-        fig, ax = plt.subplots()
-        anim = animation.FuncAnimation(fig, animate_xz,  frames = len(data), interval = len(data))
+        if flag != "3d":
+            fig, ax = plt.subplots()
+
+        if flag == "xy":
+            anim = animation.FuncAnimation(fig, animate_xy, frames = range(0, n_frames, 4), interval = 20)
+        if flag == "xz":
+            anim = animation.FuncAnimation(fig, animate_xz, frames = range(0, n_frames, 4), interval = 20)
+        if flag == "3d":
+            anim = animation.FuncAnimation(fig_3d, animate_3d, frames = range(0, n_frames, 12), interval = 20)
+
         anim.save(gif_path, fps = 60, writer = 'pillow')
+
+
+def animate_from_log_3d(log_path_agents, log_path_anchors):
+    fig = plt.figure(figsize = (20, 20))
+    ax = fig.add_subplot(projection='3d')
+
+    with open(log_path_agents, "r") as f_agents, open(log_path_anchors, "r") as f_anchors:
+        line_num = 0
+        agents_lines = [agents_line for agents_line in f_agents]
+        anchors_lines = [anchors_line for anchors_line in f_anchors]
+
+        for line_agents, line_anchors in zip(agents_lines, anchors_lines):
+            line_num += 1
+            print(f"line number: {line_num}")
+            circle_colors = ["blue", "green", "red"]
+            ax.clear()
+
+            ax.set_xlabel('x')
+            ax.set_ylabel('y')
+            ax.set_zlabel('z')
+
+            ax.set_xlim(x_bounds)
+            ax.set_ylim(y_bounds)
+            ax.set_zlim(z_bounds)
+
+            ax.set_aspect('equal', adjustable='box')
+
+            nums_agents = [float(item) for item in line_agents.rstrip().split(' ')]
+            nums_anchors = [float(item) for item in line_anchors.rstrip().split(' ')]
+
+            agents = np.array(nums_agents).reshape(-1, 12)
+            anchors = np.array(nums_anchors).reshape(-1, 12)
+
+            for agent in agents:
+                x, y, z, dx, dy, dz, ux, uy, uz, peer_state, family_id, R_vis = agent
+                circle_color = circle_colors[int(peer_state)]
+                point_color = family_colors[int(family_id)]
+
+                ax.scatter(x, y, z, s=s_point, color=point_color)
+
+            for anchor in anchors:
+                x, y, z, dx, dy, dz, ux, uy, uz, peer_state, family_id, R_vis = anchor
+                circle_color = circle_colors[int(peer_state)]
+                point_color = family_colors[int(family_id)]
+
+                ax.scatter(x, y, z, s=s_anchor, color="red")
+
+                #plt.gca().add_artist(circle)
+
+            plt.pause(0.001)
+
+        plt.show()
+
+
+def animate_3d(i):
+    print(f"animate: {i}/{n_frames}")
+    ax_3d.clear()
+
+    ax_3d.set_xlabel('x')
+    ax_3d.set_ylabel('y')
+    ax_3d.set_zlabel('z')
+
+    ax_3d.set_xlim(x_bounds)
+    ax_3d.set_ylim(y_bounds)
+    ax_3d.set_zlim(z_bounds)
+
+    ax_3d.set_aspect('equal', adjustable='box')
+    nums_agents, nums_anchors = data[i]
+
+    agents = np.array(nums_agents).reshape(-1, 12)
+    anchors = np.array(nums_anchors).reshape(-1, 12)
+
+    for agent in agents:
+        x, y, z, dx, dy, dz, ux, uy, uz, peer_state, family_id, R_vis = agent
+        point_color = family_colors[int(family_id)]
+        ax_3d.scatter(x, y, z, s=s_point, color=point_color)
+
+    for anchor in anchors:
+        x, y, z, dx, dy, dz, ux, uy, uz, peer_state, family_id, R_vis = anchor
+        point_color = family_colors[int(family_id)]
+        ax_3d.scatter(x, y, z, s=s_anchor, color="red")
 
 
 #process()
 #animate_from_log(log_path_agents, log_path_anchors, axes="xy")
-create_gif(log_path_agents, log_path_anchors, gif_path)
+#animate_from_log_3d(log_path_agents, log_path_anchors)
+#create_gif(log_path_agents, log_path_anchors, gif_path, flag="3d")
